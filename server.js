534ca272284bf6e8cfd3e9c08e287bb12784d5dd
joao <joao@streethawk.co>
2020-12-08 16:15:07 +1100

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+import serveStatic from 'serve-static'
+import history from 'connect-history-api-fallback'
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(history())
+app.use(serveStatic(`${__dirname}/dist/spa`))
+app.listen(port)
