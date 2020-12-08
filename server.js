48528ca3b169f0c1aacee09b7b2a594924508c31
joao <joao@streethawk.co>
2020-12-08 16:40:31 +1100

    Undo

---
+++ server.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const
+  express = require('express'),
+  serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
+  port = process.env.PORT || 5000
+
+const app = express()
+
+app.use(history())
+app.use(serveStatic(`${__dirname}/dist/spa`))
+app.listen(port)
